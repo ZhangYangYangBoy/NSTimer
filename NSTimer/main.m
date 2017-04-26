4a26b2def30a68e7db84ac6e2e07d461e8d72970
yangyangzhang <zyydeveloper@163.com>
2017-04-26 14:24:56 +0800

    Initial Commit

---
+++ NSTimer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSTimer
+//
+//  Created by Tech-zhangyangyang on 2017/4/26.
+//  Copyright © 2017年 Tech-zhangyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
